ed8aec68d8aaeb0eb33f52566271a9ae844b68db
leandroluizpereira <leandro.2306@hotmail.com>
2021-04-05 11:33:45 -0300

    delta

--- NeatBeans/Intermediario/Orientação a objeto/BhaskaraWeb/src/java/br/Bhaskara/calcular/Bhaskara.java
+++ NeatBeans/Intermediario/Orientação a objeto/BhaskaraWeb/src/java/br/Bhaskara/calcular/Bhaskara.java
@@ -1,10 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.Bhaskara.calcular;
 
 public class Bhaskara {
+    int a,b,c;
+    double delta,raizPositiva,raizNegativa;
+    //implementar toda lógica...
+    
+    /*
+        
+    */
     
 }
