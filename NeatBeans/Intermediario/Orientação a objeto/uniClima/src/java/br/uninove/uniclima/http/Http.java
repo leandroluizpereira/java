49a739153f76c4d840ffce5b00d71b95566606d6
leandroluizpereira <leandro.2306@hotmail.com>
2021-04-19 11:27:04 -0300

    create

---
+++ NeatBeans/Intermediario/Orientação a objeto/uniClima/src/java/br/uninove/uniclima/http/Http.java
@@ -0,0 +1,44 @@
+
+package br.uninove.uniclima.http;
+   
+import com.google.gson.JsonSyntaxException;
+import com.uninove.uniclima.tempo.Tempo;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+
+public class Http {
+    public static Tempo getClima(String cidade){
+        try {
+            String url = "http://api.openweathermap.org/data/2.5/weather";
+            String charset = StandardCharsets.UTF_8.name();
+            String appid =""; // chave api
+            String units ="metric";
+            String lang ="pt_br";
+            String query = String.format("q=%s&appid=%s&units=%s&lang=%s",
+                    URLEncoder.encode(cidade, charset),
+                    URLEncoder.encode(appid, charset),
+                    URLEncoder.encode(units, charset),
+                    URLEncoder.encode(lang, charset));
+            URLConnection conn = new URL(url +"?"+ query).openConnection();
+            conn.setRequestProperty("Accept-Charset",charset);
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded;charset="+charset);
+            Tempo t;
+            try(InputStream resp = conn.getInputStream()){
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(resp, Charset.forName("UTF-8")));
+            }
+        }catch(JsonSyntaxException | IOException E){
+            
+    }
+       return null;
+    }
+        
+}
